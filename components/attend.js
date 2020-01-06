f96b3f70bf441dca4ce212aa98a32949f460e1c7
saminightshift <sami@nightshift.media>
2020-01-06 17:22:19 +0000

    About and Attend components edited

---
+++ components/attend.js
@@ -0,0 +1,24 @@
+import react from React
+
+const Attend = () => (
+
+    <attend>
+        <h1>Why Attend?</h1>
+
+        <h4>Get the most from your notifications platform</h4>
+        <p>Learn how to get the most out of the notifications platform, and leave with actionable insights to enhance the way you communicate with your passengers.</p>
+
+        <h4>Network with peers from the world's leading travel companies</h4>
+        <p>Meet like-minded individuals who are as passionate as you are about making passenger experience the best it can be.</p>
+
+        <h4>Understand and influence the 15below product roadmap</h4>
+        <p>We want to hear the challenges you face day-to-day – and most importantly – how we can help.</p>
+    </attend>
+
+)
+
+
+
+
+
+export default attend
