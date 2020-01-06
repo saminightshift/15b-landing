bfc6c1d108cf5c3c3b12189724994bd47853ab0d
saminightshift <sami@nightshift.media>
2020-01-06 17:28:30 +0000

    Attend component edits

--- components/attend.js
+++ components/attend.js
@@ -13,8 +13,106 @@ const Attend = () => (
 
         <h4>Understand and influence the 15below product roadmap</h4>
         <p>We want to hear the challenges you face day-to-day – and most importantly – how we can help.</p>
-    </attend>
 
+        <h4>Take part in an immersive, fully interactive format</h4>
+        <p>No death-by-Powerpoint here! Enjoy three value-packed days full of deep-dive breakout sessions, panels, live case studies, interactive workshops, and more.</p>
+
+        <h4>Visit our offices and meet the team</h4>
+        <p>Meet the whole 15below team in our new home, work from our Brighton offices or join our optional ‘how to’ sessions.</p>
+
+        <h4>Take in the sights and sounds of Brighton</h4>
+        <p>Brighton will be home to two fantastic arts festivals at the same time as the conference, so why not extend your stay and find out why Brighton has been named “London by the sea”.</p>
+
+        <h4>Celebrate with us</h4>
+        <p>We’re turning 20 and would love for you to be here to celebrate the occasion with us! Join us for food, drink, awards, and entertainment.</p>
+
+
+        <style jsx>{`
+     
+     h3 {
+      font-family: "Gotham A", "Gotham B", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+      font-weight: 400;
+     }
+     
+      .signup {
+        padding: 18px 30px 24px;
+      }
+
+      .hero-banner-image { 
+        width: 100%;
+        height: 100%;
+        max-width: 2560px;
+        margin: 0;
+        
+      }
+      
+      .event-banner { 
+        position: absolute;
+        display: block;
+        left: 50%; 
+        -webkit-transform: translateX(-50%);
+        transform: translateX(-50%);
+        top: 30px;
+        width: 640px;
+        background: url('/iStock_000028193904XLarge33.jpg') no-repeat;
+      }
+      
+      h1 {
+        font-family: "Gotham A", "Gotham B", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+        font-weight: 500 !important;
+      }
+      
+      .hero {
+        width: 100%;
+        color: #fff;
+        height: 100%;
+        background: url('/iStock_000028193904XLarge33.jpg') no-repeat 1%;
+      }
+      
+      .title {
+        font-family: "Gotham A", "Gotham B", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+      .row {
+        max-width: 880px;
+        margin: 80px auto 40px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+      .card {
+        padding: 18px 18px 24px;
+        width: 220px;
+        text-align: left;
+        text-decoration: none;
+        color: #434343;
+        border: 1px solid #9b9b9b;
+      }
+      .card:hover {
+        border-color: #067df7;
+      }
+      .card h3 {
+        margin: 0;
+        color: #067df7;
+        font-size: 18px;
+      }
+      .card p {
+        margin: 0;
+        padding: 12px 0 0;
+        font-size: 13px;
+        color: #333;
+      }
+    `}</style>
+
+    </attend>
 )
 
 
